fda2081ced4d1f02394157d1c28308eac6e13c93
ryanovovo <ryanovovo@gmail.com>
2022-02-10 10:27:02 +0800

    更改函數名稱compare -> diff, 將init和guess重複的部分分離，提高執行效率

--- wordle_solver.cpp
+++ wordle_solver.cpp
@@ -2,11 +2,10 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cmath>
 using namespace std;
 
 
-inline int compare_two_strings(const string &lhs, const string &rhs){
+inline int diff_two_strings(const string &lhs, const string &rhs){
 	int res = 0;
 	int pow = 1;
 	vector<int> is_used_lhs(5, 0);
@@ -57,22 +56,23 @@ vector<vector<int>> all_words_dist(vector<string> &words){
 	int n = words.size();
 	vector<vector<int>> res(n, vector<int>(242, 0));
 	for(int i = 0; i < n; i++){
+		/*
 		if(i % 100 == 0){
 			cout << (double)i /  (double)n * 100 << '%' << endl;
 		}
+		*/
 		for(int j = 0; j < n; j++){
-			int diff = compare_two_strings(words[i], words[j]);
+			int diff = diff_two_strings(words[i], words[j]);
 			res[i][diff]++;
 		}
 	}
 	return res;
 }
 
-int guess(vector<string> &allPossibleWords){
-	auto words_dist = all_words_dist(allPossibleWords);
+int guess(vector<string> &allPossibleWords, vector<vector<int>> &words_dist){
   	double min_stdev = 99999.0;
   	int guess_idx = 0;
-  	for(int i = 0; i < allPossibleWords.size(); i++){
+  	for(unsigned int i = 0; i < allPossibleWords.size(); i++){
   		double stdev = standard_deviation(words_dist[i]);
   		if(stdev < min_stdev){
   			min_stdev = stdev;
@@ -96,12 +96,13 @@ int guess(vector<string> &allPossibleWords){
   		pow *=3;
   	}
   	for(auto word : allPossibleWords){
-  		if(compare_two_strings(word, allPossibleWords[guess_idx]) == result){
+  		if(diff_two_strings(word, allPossibleWords[guess_idx]) == result){
   			tmp.push_back(word);
   		}
   	}
   	allPossibleWords.clear();
   	allPossibleWords = tmp;
+  	words_dist = all_words_dist(allPossibleWords);
   	if(result == 242){
   		cout << "Congratulations!" << endl;
   		return 0;
@@ -110,8 +111,8 @@ int guess(vector<string> &allPossibleWords){
 }
 
 
-int init(vector<string> &allPossibleWords){
-	vector<vector<int>> words_dist(allPossibleWords.size(), vector<int>(242));
+void init(vector<vector<int>> &words_dist){
+	// vector<vector<int>> words_dist(allPossibleWords.size(), vector<int>(242));
 	ifstream dists;
 	dists.open("/Users/ryanovovo/Documents/wordle-solver/all_words_dist.txt");
 	if(dists.is_open()){
@@ -140,43 +141,7 @@ int init(vector<string> &allPossibleWords){
 		}
 	}
 	dists.close();
-	double min_stdev = 99999.0;
-  	int guess_idx = 0;
-  	for(int i = 0; i < allPossibleWords.size(); i++){
-  		double stdev = standard_deviation(words_dist[i]);
-  		if(stdev < min_stdev){
-  			min_stdev = stdev;
-  			guess_idx = i;
-  		}
-  	}
-  	vector<string> tmp;
-  	string raw_result;
-  	cout << "best guess :" << allPossibleWords[guess_idx] << endl;
-  	cout << "input result" << endl;
-  	cin >> raw_result;
-  	int result = 0;
-  	int pow = 1;
-  	for(int i = 0; i < 5; i++){
-  		if(raw_result[i] == 'o'){
-  			result += pow*2;
-  		}
-  		else if(raw_result[i] == '_'){
-  			result += pow;
-  		}
-  		pow *=3;
-  	}
-  	for(auto word : allPossibleWords){
-  		if(compare_two_strings(word, allPossibleWords[guess_idx]) == result){
-  			tmp.push_back(word);
-  		}
-  	}
-  	allPossibleWords.clear();
-  	allPossibleWords = tmp;
-  	if(result == 242){
-  		cout << "Congratulations!" << endl;
-  		return 0;
-  	}
-  	return 1;
+	return;
 }
 
 int main () {
@@ -196,9 +161,10 @@ int main () {
   	cout << "start" << endl;
   	auto allPossibleWords_cpy = allPossibleWords;
   	while(true){
+  		vector<vector<int>> words_dist(allPossibleWords.size(), vector<int>(242));
   		allPossibleWords_cpy = allPossibleWords;
-  		init(allPossibleWords_cpy);
-  		while(guess(allPossibleWords_cpy));
+  		init(words_dist);
+  		while(guess(allPossibleWords_cpy, words_dist));
   	}
   	
 }
